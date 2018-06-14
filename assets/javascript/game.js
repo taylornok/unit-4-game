005b57b75c5a62ccef224c79f495aa67f82f3d6b
taylornok <taylorowenskees@outlook.com>
2018-06-14 00:26:27 -0700

    Set Variables, defining game functions

---
+++ assets/javascript/game.js
@@ -0,0 +1,70 @@
+$(document).ready(function(){
+
+        // declare win variable to keep tally of wins
+        var wins = 0;
+        // put results into #wins span
+        $('#wins').text(wins);
+        //declare losses variable to keep tally of losses
+        var losses = 0;
+        // put results into #losses span
+        $('#losses').text(losses);
+        // initialize userTotal
+        var userTotal = 0;
+        
+
+
+        //declare the random variables
+        //random number to guess at the start of the game between 19-120
+        var guessRandom = Math.floor(Math.random() * 120-19) +19;
+        //put the random number into the empty #randomNumber div
+        $('#randomNumber').text(guessRandom);
+
+        //assigning random numbers to four crystals 1-12
+        var crystal1 = Math.floor(Math.random()*12+1);
+        var crystal2 = Math.floor(Math.random()*12+1);
+        var crystal3 = Math.floor(Math.random()*12+1);
+        var crystal4 = Math.floor(Math.random()*12+1);
+        
+
+        console.log('Random', guessRandom)
+        console.log(crystal1);
+        console.log(crystal2);
+        console.log(crystal3);
+        console.log(crystal4);
+
+
+        //set up the .onclick actions for the crystals
+
+        $('#crystal1').on ('click', function (){
+                userTotal = userTotal + crystal1;
+                console.log("New userTotal= " + userTotal);
+                $('#total-score').text(userTotal); 
+                      //sets the win or lose conditions
+                    if (userTotal == guessRandom){
+                      yay();
+                    }
+                    else if ( userTotal > Random){
+                      loser();
+                    }   
+              })  
+       
+
+        // function start  () {
+        //         Random=Math.floor(Math.random()*101+19);
+        //         console.log(Random)
+        //         $('#randomNumber').text(Random);
+        //         var crystal1 = Math.floor(Math.random()*12+1);
+        //         var crystal2 = Math.floor(Math.random()*12+1);
+        //         var crystal3 = Math.floor(Math.random()*12+1);
+        //         var crystal4 = Math.floor(Math.random()*12+1);
+        //         userTotal= 0;
+        //         $('#total-score').text(userTotal);
+        // }
+
+        
+        
+        
+
+
+
+});
